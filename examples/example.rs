7fde2dbb14ec04e2e5c0e703752958451a95c867
Fiana Fortressia <fortressnordlys@outlook.com>
2022-12-08 14:53:53 +0700

    add load_icon from bevy shell template to ensure we can use inochi_bevy with another thing being rendered
    Signed-off-by: Fiana Fortressia <fortressnordlys@outlook.com>

--- examples/example.rs
+++ examples/example.rs
@@ -1,3 +1,4 @@
+use bevy::prelude::*;
 use bevy::app::App;
 use bevy::DefaultPlugins;
 use inochi_bevy::Inochi2DPlugin;
@@ -5,5 +6,14 @@ fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
     app.add_plugin(Inochi2DPlugin);
+    app.add_startup_system(load_icon);
     app.run();
+}
+
+fn load_icon(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("bevy.png"),
+        ..default()
+    });
 }
